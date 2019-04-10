f37c8613e1d4e8a07046ff774de1afae9e4f762d
linweiyuan <root@linweiyuan.com>
2019-04-10 17:12:18 +0800

    Zookeeper

--- src/main/java/com/linweiyuan/dubbo/DubboProviderApplication.java
+++ src/main/java/com/linweiyuan/dubbo/DubboProviderApplication.java
@@ -1,4 +1,4 @@
-package com.linweiyuan.dubboprovider;
+package com.linweiyuan.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
